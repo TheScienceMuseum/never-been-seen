d4c7ddeb7a7274ed1d44a17c3c79527e26ace838
Jamie Unwin <jamieunwin@gmail.com>
2021-02-12 15:49:06 +0000

    Update first.js
    Update code to use more performant API

--- docs/first.js
+++ docs/first.js
@@ -1,26 +1,23 @@
 var request = new XMLHttpRequest();
 var imghost = 'https://s3-eu-west-1.amazonaws.com/smgco-images/images/';
+var pageprefix = 'https://collection.sciencemuseumgroup.org.uk/objects/';
 
 request.addEventListener('load', function(e) {
-    // response comes back as "{data: [{attributes: {locations: [{...}], summary_title: '...'} ...}, {} ...]}"
-    JSON.parse(this.responseText).data.forEach(function(el) {
-    
-	if (el.attributes.admin && el.attributes.admin.analytics && el.attributes.admin.analytics.count && el.attributes.admin.analytics.count.total) {
-            getObject();
-            return;
-	}
 
-    
-    document.getElementById('title').innerHTML = '<h1>Be the first to see <a href="' + el.links.self + '" target="_blank">object  ' + el.attributes.identifier[0].value + '</a> online or view <a href="index.html">another object</a>.</h1>';
-    document.getElementById('pix').innerHTML = '<a href="' + el.links.self + '" target="_blank"><img alt="" src="' + imghost + el.attributes.multimedia[0].processed.small_thumbnail.location + '"></a>';
+  el = JSON.parse(this.responseText)
 
-  });
+  identifier = el._source.identifier[0].value
+  pageurl = pageprefix + el._id; 
+  imgurl = imghost + el._source.multimedia[0].processed.small_thumbnail.location;
+
+  document.getElementById('title').innerHTML = '<h1>Be the first to see <a href="' + pageurl + '" target="_blank">object  ' + identifier + '</a> online or view <a href="index.html">another object</a>.</h1>';
+  document.getElementById('pix').innerHTML = '<a href="' + pageurl + '" target="_blank"><img alt="" src="' + imgurl + '"></a>';
 });
 
 // Get a random object
 function getObject () {
-  request.open('GET', 'https://collection.sciencemuseumgroup.org.uk/search/images/?random=1');
-  request.setRequestHeader('accept', 'application/json');
+  request.open('GET', 'https://collection.sciencemuseumgroup.org.uk/neverbeenseen');
   request.send();
 }
+
 getObject();
